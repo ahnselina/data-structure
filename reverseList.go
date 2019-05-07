f7fe674bb987a337c055da9a7940b173da6a98c3
ahnselina <ahnselina@qq.com>
2019-05-07 11:53:10 +0800

    leetcode 206 reverse List

---
+++ reverseList.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    cur := head
+    var pre *ListNode
+    var tmp *ListNode
+    for cur != nil {
+        tmp = cur.Next
+        cur.Next = pre
+        pre = cur
+        cur = tmp
+    }
+    
+    return pre
+}
